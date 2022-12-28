23db09da3781936805b92139dc9f4c05e9362b4a
consultant <faisel.ali1@yahoo.com>
2022-12-28 11:59:14 -0500

    implemented network request

--- app/src/main/java/com/example/booktest/MainActivity.kt
+++ app/src/main/java/com/example/booktest/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.booktest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import com.example.booktest.databinding.SearchFragmentBinding
+import com.example.booktest.model.view.Communicator
+import com.example.booktest.model.view.DisplayFragment
+import com.example.booktest.model.view.SearchFragment
 
-class MainActivity : AppCompatActivity() {
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity(), Communicator {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_search, SearchFragment())
+            .commit()
+
+    }
+
+    override fun sendDataToSearch(
+        bookTitle: String,
+        bookFilter: String,
+        bookType: String) {
+        // TODO("Not yet implemented")
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_display, DisplayFragment
+                .newInstance(bookTitle, bookFilter, bookType))
+            .addToBackStack("")
+            .commit()
     }
 }
--- app/src/main/java/com/example/booktest/model/remote/Network.kt
+++ app/src/main/java/com/example/booktest/model/remote/Network.kt
@@ -1,5 +1,7 @@
 package com.example.booktest.model.remote
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -14,7 +16,17 @@ class Network {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(createOkHttpClient())
             .build()
     }
+    private fun createOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
 
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+        return client
+    }
 }
